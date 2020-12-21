75396c6577a192e9742bb6d4f6aa55d641c7dd5c
fathitarek <fathitarek208@gmail.com>
2020-12-21 15:54:09 +0200

    customer auth

--- routes/web.php
+++ routes/web.php
@@ -62,3 +62,17 @@ Route::GET('our-products-with-sub_category/{sub_category_id}', 'productsPageCont
 
 
 
+
+Route::group(['prefix' => 'customer'], function () {
+  Route::get('/login', 'CustomerAuth\LoginController@showLoginForm')->name('login');
+  Route::post('/login', 'CustomerAuth\LoginController@login');
+  Route::post('/logout', 'CustomerAuth\LoginController@logout')->name('logout');
+
+  Route::get('/register', 'CustomerAuth\RegisterController@showRegistrationForm')->name('register');
+  Route::post('/register', 'CustomerAuth\RegisterController@register');
+
+  Route::post('/password/email', 'CustomerAuth\ForgotPasswordController@sendResetLinkEmail')->name('password.request');
+  Route::post('/password/reset', 'CustomerAuth\ResetPasswordController@reset')->name('password.email');
+  Route::get('/password/reset', 'CustomerAuth\ForgotPasswordController@showLinkRequestForm')->name('password.reset');
+  Route::get('/password/reset/{token}', 'CustomerAuth\ResetPasswordController@showResetForm');
+});
